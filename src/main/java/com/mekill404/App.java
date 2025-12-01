c21028ffd7c4acc3a3c4ced176920e4d69d788eb
mekill404 <hei.mahery.55@gmail.com>
2025-12-01 13:42:35 +0100

    Refactor property loading order in App.java for improved clarity

--- src/main/java/com/mekill404/App.java
+++ src/main/java/com/mekill404/App.java
@@ -10,16 +10,16 @@ public class App
   public static void main( String[] args ) throws Exception
   {
     Properties properties = new Properties();
-    String url = properties.getProperty("jdbc.url");
-    String username = properties.getProperty("jdbc.username");
-    String password = properties.getProperty("jdbc.password");
+    
 
     try(FileInputStream fis = new FileInputStream("conf.properties")) {
       properties.load(fis);
     }
-
-    Class.forName(properties.getProperty("jdbc.driver.class"));
+    String url = properties.getProperty("jdbc.url");
+    String username = properties.getProperty("jdbc.username");
+    String password = properties.getProperty("jdbc.password");
     
+    Class.forName(properties.getProperty("jdbc.driver.class"));
     try (Connection connection = DriverManager.getConnection(url, username, password)){
       System.out.println("Connected to the database!");
       String requetteSQL = "CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name VARCHAR(100), email VARCHAR(100))";
