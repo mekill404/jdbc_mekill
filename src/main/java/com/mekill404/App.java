3178810df8d99f0612aa9dacd02792cec956f5cf
mekill404 <hei.mahery.55@gmail.com>
2025-12-01 10:59:35 +0100

    initialisation du projet

---
+++ src/main/java/com/mekill404/App.java
@@ -0,0 +1,13 @@
+package com.mekill404;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
