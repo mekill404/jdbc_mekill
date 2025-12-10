1dced1285e6468eac4c77309a5b70e2bbe440776
mekill404 <hei.mahery.55@gmail.com>
2025-12-10 05:58:27 +0100

    refactorate(dbconnection)
    suppression des attribut static

--- src/main/java/com/mekill404/DBconnection/DBConnection.java
+++ src/main/java/com/mekill404/DBconnection/DBConnection.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class DBConnection 
 {
-  private static final String JDBC_URL = "jdbc:postgresql://localhost:5432/product_management_db";
-  private static final String USER = "product_manager_user";
-  private static final String PASSWORD = "123456";
+  private final String JDBC_URL = "jdbc:postgresql://localhost:5432/product_management_db";
+  private final String USER = "product_manager_user";
+  private final String PASSWORD = "123456";
 
   public DBConnection() 
   {
